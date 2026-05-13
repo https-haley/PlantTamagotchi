9b15dba80aa1b371d4e022d3067e463a0c44c2d3
https-haley <trejohaley@gmail.com>
2026-05-13 16:40:38 -0500

    Add files via upload
    Add this to Aurdino -> libraries ->TFT_eSPI

---
+++ User_Setup.h
@@ -0,0 +1,21 @@
+#define ST7789_DRIVER
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 280
+
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   5
+#define TFT_DC   2
+#define TFT_RST  4
+#define TFT_MISO -1
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SPI_FREQUENCY 40000000

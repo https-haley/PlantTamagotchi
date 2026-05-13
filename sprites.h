8f328c16689855263ef531f99c4e6db69895ef7c
https-haley <trejohaley@gmail.com>
2026-05-13 16:35:18 -0500

    Add sprite definitions for moisture levels
    Define constants for screen dimensions and declare image arrays in PROGMEM.

---
+++ sprites.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <Arduino.h>
+
+#define MOIST1_HEIGHT 240 // pixel screen height
+#define MOIST1_WIDTH 280 //pixel screen width
+
+// Use this website to turn png to arudunio code
+// https://mischianti.org/rgb-image-to-byte-array-converter-for-arduino-tft-displays/#google_vignette
+
+const uint16_t moist1[] PROGMEM = 
+{
+  // image code here
+};
+const uint16_t moist2[] PROGMEM = 
+{
+  // image code goes here
+};
+const uint15_t wet1[] PROGMEM = 
+{
+  // image code
+};
+const unint15_t wet2[] PROGMEM =
+{
+  // image code
+};
+const unint15_t dry1[] PROGMEM = 
+{
+  // image code
+};
+const unint15_t dry2[] PROGMEM =
+{
+  // image code
+};
